774b6510873ec72559c6f3f7e1e57b445fa39e29
Brian NQC <brian.nqc@outlook.com>
2023-10-14 11:37:16 +0800

    Add videos_crawler.js to crawl videos from vtv.vn

---
+++ app/vietnam-history/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Lịch sử Việt Nam by VTV</h1>
+            <h1>Phim hoat hinh lich su Vietnam</h1>
+        </main>
+    )
+}
