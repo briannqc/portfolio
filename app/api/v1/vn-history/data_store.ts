6d2a12a63aeac219ca138281111e98f46abb87f4
Brian NQC <brian.nqc2023@gmail.com>
2023-10-15 15:47:03 +0800

    [VN-History] Implement API: GET api/v1/vn-history/watch-next?lastWatchedId=

--- app/api/v1/vn-history/data_store.ts
+++ app/api/v1/vn-history/data_store.ts
@@ -5,6 +5,7 @@ type Thumbnail = {
 }
 
 export type Video = {
+    id: number
     title: string
     url: string
     datetime: string
@@ -22,4 +23,39 @@ export async function fetchVideoLibrary(page: number): Promise<Video[]> {
     const start = (page - 1) * pageSize
     const end = start + pageSize
     return videos.slice(start, end)
+}
+
+export async function fetchWatchNextVideos(lastWatchedVideoId: number): Promise<Video[]> {
+    const videos = await readAll()
+
+    const watchNextMaxSize = 5
+    let indexOfLastWatchedVideo = indexOfVideo(videos, lastWatchedVideoId)
+    if (indexOfLastWatchedVideo < 0) {
+        indexOfLastWatchedVideo = watchNextMaxSize - 1
+    }
+
+    const watchNextVideos: Video[] = []
+    for (let i = 0; i < watchNextMaxSize && indexOfLastWatchedVideo - i >= 0; i++) {
+        watchNextVideos.push(videos[indexOfLastWatchedVideo - i])
+    }
+    return watchNextVideos
+}
+
+function indexOfVideo(videos: Video[], videoId: number) {
+    let left: number = 0;
+    let right: number = videos.length - 1;
+
+    while (left <= right) {
+        const mid: number = Math.floor((left + right) / 2);
+
+        if (videos[mid].id === videoId) {
+            return mid;
+        }
+        if (videoId > videos[mid].id) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
 }
---
+++ app/api/v1/vn-history/watch-next/route.ts
@@ -0,0 +1,25 @@
+import {NextRequest, NextResponse} from "next/server";
+import {fetchWatchNextVideos} from "@/app/api/v1/vn-history/data_store";
+
+export async function GET(request: NextRequest) {
+    const lastWatchedId = extractLastWatchedIdQuery(request)
+    const videos = await fetchWatchNextVideos(lastWatchedId)
+    return NextResponse.json(
+        {
+            meta: {
+                size: videos.length,
+            },
+            data: videos
+        },
+        {status: 200}
+    )
+}
+
+function extractLastWatchedIdQuery(request: NextRequest) {
+    const id = request.nextUrl.searchParams.get('lastWatchedId') || "0"
+    const idNumber = parseInt(id, 10)
+    if (isNaN(idNumber)) {
+        return 0
+    }
+    return idNumber
+}
