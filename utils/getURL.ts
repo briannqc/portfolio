f0b0546ed22a441806a82676e5590b79a229067d
Brian NQC <brian.nqc2023@gmail.com>
2023-10-16 00:22:55 +0800

    FIX VERCEL_URL Does not come with https://
    NEXT_PUBLIC_SITE_URL points to https://nqc.io.vn
    and should only be used in production. For preview,
    we should use VERCEL_URL

--- utils/getURL.ts
+++ utils/getURL.ts
@@ -1,8 +1,15 @@
 const IS_SERVER = typeof window === "undefined";
 export default function getURL(path: string): URL {
+    console.log(`process.env.NEXT_PUBLIC_SITE_URL: ${process.env.NEXT_PUBLIC_SITE_URL}`)
+    console.log(`process.env.VERCEL_URL: ${process.env.VERCEL_URL}`)
     const serverUrl = process.env.NEXT_PUBLIC_SITE_URL
         ? process.env.NEXT_PUBLIC_SITE_URL
-        : process.env.VERCEL_URL
+        : `https://${process.env.VERCEL_URL}`
+
+    console.log(`serverUrl: ${serverUrl}`)
+
     const baseURL = IS_SERVER ? serverUrl : window.location.origin;
+
+    console.log(`baseURL: ${baseURL}`)
     return new URL(path, baseURL)
 }
