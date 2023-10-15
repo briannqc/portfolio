748f6f2f4a2d2d3a0bdd5e901b096845e294ae15
Brian NQC <brian.nqc2023@gmail.com>
2023-10-15 12:13:38 +0800

    [VN-History] Implement API: GET api/v1/vn-history/video-library?page=

---
+++ app/api/v1/vn-history/video-library/route.ts
@@ -0,0 +1,26 @@
+import {NextRequest, NextResponse} from "next/server";
+import {fetchVideoLibrary} from "@/app/api/v1/vn-history/data_store";
+
+export async function GET(request: NextRequest) {
+    const page = extractPageQuery(request)
+    const videos = await fetchVideoLibrary(page)
+    return NextResponse.json(
+        {
+            meta: {
+                page: page,
+                size: videos.length,
+            },
+            data: videos
+        },
+        {status: 200}
+    )
+}
+
+function extractPageQuery(request: NextRequest) {
+    const page = request.nextUrl.searchParams.get('page') || "1"
+    const pageNumber = parseInt(page, 10)
+    if (isNaN(pageNumber) || pageNumber < 1) {
+        return 1
+    }
+    return pageNumber
+}
