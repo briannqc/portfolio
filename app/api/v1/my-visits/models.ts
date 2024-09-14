ae78ab1255000360681954ce9f1ed093e51395cd
Brian NQC <brian.nqc2023@gmail.com>
2024-09-14 21:36:17 +0700

    [my-visits] Add GET v1/my-visits/office-visits

--- app/api/v1/my-visits/models.ts
+++ app/api/v1/my-visits/models.ts
@@ -1,4 +1,13 @@
+import {DateTime} from "luxon";
+
 export type Place = {
     location: string;
     address: string;
+}
+
+export type OfficeVisits = {
+    arrivedAt?: DateTime
+    leftAt?: DateTime
+    visitCountThisQuarter: number
+    trackingSince?: DateTime
 }
---
+++ app/api/v1/my-visits/office-visits/route.ts
@@ -0,0 +1,8 @@
+import {NextResponse} from "next/server";
+import {retrieveOfficeVisits} from "@/app/api/v1/my-visits/data_store";
+
+
+export async function GET() {
+    const visits = await retrieveOfficeVisits()
+    return NextResponse.json(visits);
+}
