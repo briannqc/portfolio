1b6ba024b695445357c265c5e4895ae60616dd42
Brian NQC <brian.nqc2023@gmail.com>
2024-09-14 21:36:17 +0700

    [my-visits] Add GET v1/my-visits/office-visits

--- app/api/v1/my-visits/data_store.ts
+++ app/api/v1/my-visits/data_store.ts
@@ -1,5 +1,7 @@
+import {DateTime} from 'luxon';
+
 import {sql} from '@/app/api/db';
-import {Place} from "@/app/api/v1/my-visits/models";
+import {OfficeVisits, Place} from "@/app/api/v1/my-visits/models";
 
 enum Action {
     ARRIVED = 'ARRIVED',
@@ -20,4 +22,92 @@ async function insertVisitEvent(place: Place, action: Action): Promise<boolean> 
         VALUES ('${place.location}', '${place.address}', '${action}')
     `);
     return rowCount === 1;
+}
+
+export async function retrieveOfficeVisits(): Promise<OfficeVisits> {
+    const [firstArrival, lastDeparture, visitsCount, trackingSince] = await Promise.all([
+        retrieveEarliestArrivalToOfficeToday(),
+        retrieveLastDepartureFromOfficeToday(),
+        countVisitsToOfficeThisQuarter(),
+        retrieveFirstEverArrivalToOffice()
+    ])
+
+    return {
+        arrivedAt: firstArrival,
+        leftAt: lastDeparture,
+        visitCountThisQuarter: visitsCount,
+        trackingSince: trackingSince,
+    }
+}
+
+async function retrieveEarliestArrivalToOfficeToday(): Promise<DateTime | undefined> {
+    const startOfToday = nowInHCMC().startOf('day').toISO()
+    const {rowCount, rows} = await sql.query(`
+        SELECT timestamp
+        FROM portfolio.place_visits
+        WHERE place = 'OFFICE' AND action = '${Action.ARRIVED}' AND timestamp > '${startOfToday}'
+        ORDER BY timestamp
+        LIMIT 1;
+    `);
+
+    if (rowCount === 0) {
+        return undefined
+    }
+
+    const timestampTz = rows[0].timestamp.toISOString()
+    return DateTime.fromISO(timestampTz)
+}
+
+function nowInHCMC(): DateTime {
+    return DateTime.now().setZone('Asia/Ho_Chi_Minh')
+}
+
+async function retrieveLastDepartureFromOfficeToday(): Promise<DateTime | undefined> {
+    const startOfToday = nowInHCMC().startOf('day').toISO()
+    const {rowCount, rows} = await sql.query(`
+        SELECT timestamp
+        FROM portfolio.place_visits
+        WHERE place = 'OFFICE' AND action = '${Action.LEFT}' AND timestamp > '${startOfToday}'
+        ORDER BY timestamp DESC
+        LIMIT 1;
+    `);
+
+    if (rowCount === 0) {
+        return undefined
+    }
+
+    const timestampTz = rows[0].timestamp.toISOString()
+    return DateTime.fromISO(timestampTz)
+}
+
+async function countVisitsToOfficeThisQuarter(): Promise<number> {
+    const startOfQuarter = nowInHCMC().startOf('quarter').toISO()
+    const {rowCount, rows} = await sql.query(`
+        SELECT COUNT(DISTINCT DATE_TRUNC('day', timestamp)) AS visit_days
+        FROM portfolio.place_visits
+        WHERE place = 'OFFICE' AND action = '${Action.ARRIVED}' AND timestamp > '${startOfQuarter}'
+        GROUP BY place;
+    `);
+
+    if (rowCount === 0) {
+        return 0
+    }
+    return parseInt(rows[0].visit_days, 10)
+}
+
+async function retrieveFirstEverArrivalToOffice(): Promise<DateTime | undefined> {
+    const {rowCount, rows} = await sql.query(`
+        SELECT timestamp
+        FROM portfolio.place_visits
+        WHERE place = 'OFFICE' AND action = '${Action.ARRIVED}'
+        ORDER BY timestamp
+        LIMIT 1;
+    `);
+
+    if (rowCount === 0) {
+        return undefined
+    }
+
+    const timestampTz = rows[0].timestamp.toISOString()
+    return DateTime.fromISO(timestampTz)
 }
