c762bfddb24ed8dc22ad95006ae67eb4fea0c909
Brian NQC <8863207+briannqc@users.noreply.github.com>
2025-07-19 13:23:21 +1000

    [lotus34] Define openapi spec and expose with swagger-ui-react (#24)

---
+++ app/lotus34/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
+
+export default function Lotus34SwaggerUI() {
+    return <SwaggerUI url="/lotus34/lotus34-openapi.yaml" />
+}
