adef85989426683b7532c38c8cd99ec39a8f2f56
Brian NQC <brian.nqc2023@gmail.com>
2025-07-19 21:41:24 +1000

    [lotus34] Enhance API responses with strongly type meta

--- app/api/lotus34/v2/provinces/[code]/communes/route.ts
+++ app/api/lotus34/v2/provinces/[code]/communes/route.ts
@@ -1,23 +1,32 @@
 import {NextRequest, NextResponse} from "next/server";
 import {loadDataStore} from "@/app/api/lotus34/v2/data_store"
+import {APIResponse} from "@/app/api/lotus34/v2/response";
 
 export async function GET(
     _: NextRequest,
     {params}: { params: { code: string; } }
-) {
+): Promise<NextResponse<APIResponse>> {
     const ds = await loadDataStore()
     const communes = ds.listCommunesByProvinceCode(params.code)
     if (!communes) {
-        return NextResponse.json({}, {status: 404})
+        const resp: APIResponse = {
+            data: null,
+            meta: {
+                message: `Province Code ${params.code} not found`,
+            }
+        }
+        return NextResponse.json(resp, {status: 404})
     }
 
-    return NextResponse.json(
-        {
-            meta: {
-                size: communes.length,
-            },
-            data: communes
-        },
-        {status: 200}
-    )
+    const resp: APIResponse = {
+        data: communes,
+        meta: {
+            message: "List communes successfully",
+            page: 0,
+            total: communes.length,
+            previousPage: null,
+            nextPage: null,
+        }
+    }
+    return NextResponse.json(resp, {status: 200})
 }
